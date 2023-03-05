ab54d0667d84e2f5eecf66b068bb848b4cffe7b6
HoangNam1973 <125440619+HoangNam1973@users.noreply.github.com>
2023-03-06 00:16:55 +0700

    Bài tập tuần 5

---
+++ Bai5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Bai5 {
+	public static void main(String[] args) {
+		 int sum = 0, n;
+		    Scanner sc = new Scanner(System.in);
+		    do {
+		        System.out.println("Nhập n :");
+		        n = sc.nextInt();
+		        sum += n;
+		            
+		        if (sum > 100)
+		            break;
+		    } while (n > 0);
+		         
+		    System.out.println("Tổng = " + sum);
+	}
+}
---
+++ Bai7.java
@@ -0,0 +1,8 @@
+public class Bai7 {
+	public static void main(String[] args) {
+		int n = 20;
+		for(int i = 0; i <= n;i++) {
+			System.out.println("20 số nguyên dương đầu tiên : "+ i);
+		}
+	}
+}
